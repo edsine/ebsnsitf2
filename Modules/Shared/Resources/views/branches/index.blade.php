7a4043bb4ad2d85b42cc2dc95314a9f51982d8b8
Ayeni Ayobami <ayeniayobami2013@yahoo.com>
2023-08-01 13:36:48 +0100

    Add search to branches and departments table

--- Modules/Shared/Resources/views/branches/index.blade.php
+++ Modules/Shared/Resources/views/branches/index.blade.php
@@ -14,6 +14,23 @@
                     </a>
                 </div>
             </div>
+            <div class="row mb-2">
+                <div class="col-sm-3">
+                    <form method="get" action="" class="navbar-search mr-4">
+                        <div class="input-group">
+                            <input type="text" name="search" value="{{ \Request::get('search', '') }}"
+                                class="form-control bg-light border-0 small" placeholder="@lang('Search Branches list..')" aria-label="Search"
+                                aria-describedby="basic-addon2">
+
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="submit">
+                                    <i class="fas fa-search fa-sm"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </section>
 
